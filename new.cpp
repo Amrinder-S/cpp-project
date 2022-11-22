cc2adaa7b62b53e0108bb234e92c6a41c9bbd786
Amrinder Singh <80534489+Amrinder-S@users.noreply.github.com>
2022-11-22 22:26:53 +0530

    playing with pointers and functions

--- new.cpp
+++ new.cpp
@@ -1,28 +1,26 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
-
-
 void test0() {
 cout<<"function 1\n";
 }
-
 void test1() {
 cout<<"function 2\n";
 }
-
 void test2() {
 cout<<"function 3\n\a";
 }
 
 int main() {
-
 void (*p[3])(void);
 p[0]=&test0;
 p[1]=&test1;
 p[2]=&test2;
-
-for(int i=0;i<3;i++)
-	(*p[i])();
+while(1)
+{
+cout<<"Enter the function to execute:";
+(p[int(getch())-49])();
+}
 
 	return 0;
 }
